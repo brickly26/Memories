c9a9fb5a53ce76d0edacfc4b7fc70ebc2c673871
brickly26 <sabburak26@gmail.com>
2022-06-21 15:29:16 -0400

    created an api endpoint that gets post messages

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,4 +1,20 @@
+import PostMessage from "../models/postMessage.js";
 
-export const getPosts = (req, res) => {
-    res.send('This Works!')
+export const getPosts = async (req, res) => {
+    try {
+        const postMessages = await PostMessage.find();
+
+        res.status(200).json(postMessages);
+    } catch (error) {
+        res.status(404).json({ message: error.message });
+    }
+}
+
+export const createPosts = (req, res) => {
+    const body = req.body;
+    try {
+
+    } catch (error) {
+
+    }
 }
