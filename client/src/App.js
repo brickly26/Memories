5df94e2ab7af6724db5938654f27f067e2ac9613
brickly26 <sabburak26@gmail.com>
2022-06-21 16:45:58 -0400

    added styles on app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
 import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
 
+import Posts from './components/Posts/Posts';
+import Form from './components/Form/Form';
 import memories from "./images/memories.png";
+import useStyles from './styles';
+
 
 const App = () => {
+    const classes = useStyles();
+
   return (
-    <Container maxidth="lg">
-      <AppBar postion="static" color="inherit">
-        <Typography variant="h2" align="center">
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} postion="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <img src={memories} alt="memories" height="60" />
+        <img className={classes.image} src={memories} alt="memories" height="60" />
       </AppBar>
       <Grow in>
         <Container>
@@ -18,7 +24,7 @@ const App = () => {
             container
             justifyContent="space-between"
             alignItems="stretch"
-            spacing={4}
+            spacing={3}
           >
               <Grid item xs={12} sm={7}>
                 <Posts />
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import Post from './Post/Post'
+
 const Posts = () => {
   return (
-    <div>Posts</div>
+    <>
+      <Post />
+      <Post />
+      <Post />
+    </>
   )
 }
 
