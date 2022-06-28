f383783de990b4b3d84f79dfc9fbdbfce82b4e9c
brickly26 <sabburak26@gmail.com>
2022-06-28 16:19:33 -0400

    added the ability to update posts and what not

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,9 +1,12 @@
+
 export default (posts = [], action) => {
     switch (action.type) {
         case 'FETCH_ALL':
             return action.payload;
         case 'CREATE':
             return [action.payload, ...posts];
+        case 'UPDATE':
+            return posts.map(post => post._id === action.payload._id ? action.payload : post)
         default:
             return posts;
     }
