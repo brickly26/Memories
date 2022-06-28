22719e5756e986e975cb530ba5ea2ac34f32ef94
brickly26 <sabburak26@gmail.com>
2022-06-28 17:09:28 -0400

    added deleting functionality

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -8,13 +8,12 @@ import useStyles from "./styles";
 const Posts = ({ setCurrentId }) => {
   const classes = useStyles();
   const posts = useSelector((state) => state.posts)
-  
-  console.log(posts)
+
   return (
     !posts.length ? <CircularProgress /> : (
-      <Grid className={classes.container} conatiner alignItems='stretch' spacing={3}>
+      <Grid className={classes.container} container alignItems='stretch' spacing={3}>
         {posts.map(post => (
-          <Grid key={post.id} item xs={12} sm={6}>
+          <Grid key={post._id} item xs={12} sm={6}>
             <Post post={post} setCurrentId={setCurrentId} />
           </Grid>
         ))}
