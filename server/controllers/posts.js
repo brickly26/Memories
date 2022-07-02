ae276c9091c26e5c6d35ef06c74c38e82146bc63
brickly26 <sabburak26@gmail.com>
2022-07-02 14:45:23 -0400

    made it so a user can only like a post once

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -49,11 +49,23 @@ export const deletePost = async (req, res) => {
 export const likePost = async (req, res) => {
     const { id: _id } = req.params;
 
+    if(!req.userId) return res.json({ message: 'Unauthenticated' });
+
     if(!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id.');
 
     const post = await PostMessage.findById(_id);
 
-    const updatedPost = await PostMessage.findByIdAndUpdate(_id, {likeCount: post.likeCount++, ...post}, { new: true })
+    const index = post.likes.findIndex((id) => id === String(req.userId));
+
+    if(index === -1) {
+        // like the post
+        post.likes.push(req.userId);
+    } else {
+        // dislike the post
+        post.likes = post.likes.filter((id) => id !== String(req.UserId));
+    }
+
+    const updatedPost = await PostMessage.findByIdAndUpdate(_id, post, { new: true })
 
     res.json(updatedPost)
 }
