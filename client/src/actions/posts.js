bb872ddad0798c92da73a64b4a217f6f775dbdc8
brickly26 <sabburak26@gmail.com>
2022-07-03 11:26:26 -0400

    added post details page that also shows similar posts to the one you are on

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,21 +1,31 @@
-import { FETCH_ALL, FETCH_BY_SEARCH, START_LOADING, END_LOADING, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_POST, FETCH_BY_SEARCH, START_LOADING, END_LOADING, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 import * as api from '../api';
 
 // Action Creators
+
+export const getPost = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: START_LOADING });
+        const { data } = await api.fetchPost(id);
+
+        dispatch({ type: FETCH_POST, payload: data });
+        dispatch({ type: END_LOADING })
+    } catch (error) {
+        console.log(error.message)
+    }
+}
 
 export const getPosts = (page) => async (dispatch) => {
     try {
         dispatch({ type: START_LOADING });
         const { data } = await api.fetchPosts(page);
 
-        console.log(data)
-
         dispatch({ type: FETCH_ALL, payload: data });
         dispatch({ type: END_LOADING })
     } catch (error) {
         console.log(error.message)
     }
-}
+};
 
 export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     try {
@@ -29,10 +39,12 @@ export const getPostsBySearch = (searchQuery) => async (dispatch) => {
     }
 };
 
-export const createPost = (post) => async (dispatch) => {
+export const createPost = (post, history) => async (dispatch) => {
     try {
         dispatch({ type: START_LOADING });
         const { data } = await api.createPost(post);
+
+        history.push(`/posts/${data._id}`)
 
         dispatch({ type: CREATE, payload: data })
     } catch (error) {
