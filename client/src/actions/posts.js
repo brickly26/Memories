98b798ae444f98f167262cdf472dedc941d15320
brickly26 <sabburak26@gmail.com>
2022-07-02 22:18:03 -0400

    added search functionality to the backend that allows users to search for post using the title and the tags

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,4 +1,4 @@
-import { FETCH_ALL, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
+import { FETCH_ALL, FETCH_BY_SEARCH, CREATE, UPDATE, DELETE } from '../constants/actionTypes';
 import * as api from '../api';
 
 // Action Creators
@@ -10,8 +10,17 @@ export const getPosts = () => async (dispatch) => {
     } catch (error) {
         console.log(error.message)
     }
-
 }
+
+export const getPostsBySearch = (searchQuery) => async (dispatch) => {
+    try {
+        const { data: { data } } = await api.fetchPostsBySearch(searchQuery);
+
+        dispatch({ type: FETCH_BY_SEARCH, payload: data })
+    } catch (error) {
+        console.log(error);
+    }
+};
 
 export const createPost = (post) => async (dispatch) => {
     try {
