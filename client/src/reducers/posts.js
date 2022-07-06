036198fa2be50749fe4b995e4072e5d907c418d2
brickly26 <sabburak26@gmail.com>
2022-07-06 19:14:54 -0400

    more code clean up

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -7,14 +7,15 @@ export default (state = { isLoading: true, posts: [] }, action) => {
         case END_LOADING:
             return { ...state, isLoading: false }
         case FETCH_BY_SEARCH:
+        case FETCH_BY_CREATOR:
             return {
                 ...state,
-                posts: action.payload
+                posts: action.payload.data
             };
         case FETCH_POST:
             return {
                 ...state,
-                post: action.payload
+                post: action.payload.post
             };
         case FETCH_ALL:
             return { ...state,
@@ -26,7 +27,7 @@ export default (state = { isLoading: true, posts: [] }, action) => {
             return { ...state, posts: state.posts.map(post => post._id === action.payload._id ? action.payload : post) }
         case COMMENT: 
             return { ...state, posts: state.posts.map((post) => {
-                if (post._id === action.payload._id) return action.payload;
+                if (post._id == +action.payload._id) return action.payload;
                 
                 return post;
             })}
