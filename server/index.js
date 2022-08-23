cafe3aba1fd934584372e71780821d548b1288a5
brickly26 <sabburak26@gmail.com>
2022-08-23 15:01:14 -0400

    added package.json file to main project

--- server/index.js
+++ server/index.js
@@ -14,6 +14,10 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
+
 app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 
