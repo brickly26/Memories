3fdce14b35803bf9b266da359717e90c67916b08
brickly26 <sabburak26@gmail.com>
2022-08-24 17:08:01 -0400

    type system error

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -34,7 +34,7 @@ const Navbar = () => {
     }
 
     setUser(JSON.parse(localStorage.getItem('profile')));
-  }, [location, user.token]);
+  }, [location]);
 
   return (
     <AppBar className={classes.appBar} position="static" color="inherit">
